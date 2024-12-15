cae3b3670302f0b2bed4fbb740b91b5521fb41a4
Ilya Stepanov <ilya.stepanov.dev@gmail.com>
2024-12-15 11:15:46 +0300

    The class has been modified, the logic for working with history has been moved to a separate class.

--- duck_ai/api.py
+++ duck_ai/api.py
@@ -3,27 +3,25 @@ import re
 from types import TracebackType
 from typing import Self
 import aiohttp
-from .models import ModelType, Data , Message, Role
+from .models import Message, Role
+from .history.history import History
+from .history.json_history import JsonHistory
+
 
 type json_str = str
 
 URL = 'https://duckduckgo.com/duckchat/v1/'
 
+# TODO Придумать, что сделать с save_storage()
+
 class AiChat:
     def __init__(self,
-        model: ModelType = ModelType.GPT4o,
-        messages: list[Message] | None = None,
-        session: aiohttp.ClientSession | None = None,
-        vqd: str = '1'
+                session: aiohttp.ClientSession | None = None,
+                history: History | None = None
         ) -> None:
+
         self._session = session or aiohttp.ClientSession()
-        self.vqd = vqd 
-
-        if not messages:
-            self.data = Data(model=model, messages=[])
-        else:
-            self.data = Data(model=model, messages=messages)
-
+        self.history = history
 
     async def __aenter__(self) -> Self:
         return self
@@ -57,36 +55,38 @@ class AiChat:
     async def _get_vqd(self) -> None:
         async with self._session.get(
             url = URL+'status',
-            headers = {'x-vqd-accept': self.vqd},
+            headers = {'x-vqd-accept': self.history.data.vqd},
+
         ) as response:
             
             if response.status != 200:
                 raise Exception(f"Failed to initialize chat: {response.status} {await response.text()}")
             if "x-vqd-4" in response.headers:
-                self.vqd = response.headers["x-vqd-4"]
+                self.history.data.vqd = response.headers["x-vqd-4"]
+                self.history.save_storage()
 
 
-    async def send_request(self, message: str) ->str :
-        self.data.messages.append(Message(role=Role.user, content=message))
+    async def send_request(self, message: str) -> str:
+        self.history.add_message(Message(role=Role.USER, content=message))
 
-        if self.vqd == '1':
+        if self.history.data.vqd == '1':
             await self._get_vqd()
         
         async with self._session.post(
             url = URL + 'chat', 
             headers = {
-                "x-vqd-4": self.vqd,
+                "x-vqd-4": self.history.data.vqd,
                 "Content-Type": "application/json",
                 "Accept": "text/event-stream"
             }, 
-            data=self.data.model_dump_json(),
+            data=self.history.get_data_json(),
         ) as response:     
                    
             if response.status != 200:
                 raise Exception(f"Failed to send message: {response.status} {await response.text()}")
-            self.vqd = response.headers["x-vqd-4"]
+            self.history.data.vqd = response.headers["x-vqd-4"]
 
             response = await response.text()
             response = self._glue_response(response)
-            self.data.messages.append(Message(role=Role.assistant, content=response))
+            self.history.add_message(Message(role=Role.ASSISTANT, content=response))
             return response
